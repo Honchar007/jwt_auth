9a7df770775caba2d12f7e54f04b819bc666b308
Honchar007 <andrey2002335@gmail.com>
2022-06-28 00:21:04 +0300

    login and logout implemented

---
+++ server/middlewares/error-middleware.js
@@ -0,0 +1,9 @@
+const ApiError = require('../exceptions/api-error');
+
+module.exports = function (err, req, res, next) {
+    console.log(err);
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message, errors: err.errors})
+    } else return res.status(500).json({message: 'Something wents wrong...'})
+
+};
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,30 +4,28 @@ const uuid = require('uuid');
 const mailService = require('../service/mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
+const ApiError = require('../exceptions/api-error');
 
 class UserService {
   async registration(email, password) {
-    try {
-      const candidate = await UserModel.findOne({ email: 'email' });
+    const candidate = await UserModel.findOne({ email: 'email' });
 
-      if (candidate) {
-        throw new Error('This email had been already chosen');
-      }
-      const hashPassword = await bcrypt.hash(password, 3);
-      const activationLink = uuid.v4();
+    if (candidate) {
+      throw ApiError.BadRequest('This email had been already chosen');
+    }
 
-      const user = await UserModel.create({ email, password: hashPassword, activationLink });
-      await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
-      const userDto = new UserDto(user);
-      const tokens = tokenService.generateTokens( { ...userDto });
-      await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
 
-      return {
-        ...tokens,
-        user: userDto,
-      }
-    } catch (error) {
-      console.log(error);
+    const user = await UserModel.create({ email, password: hashPassword, activationLink });
+    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens( { ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
     }
   }
 
@@ -35,9 +33,30 @@ class UserService {
     console.log('asd');
     const user = await UserModel.findOne({ activationLink });
     console.log(activationLink);
-    if (!user) throw new Error('Invalid activation link');
+    if (!user) throw ApiError.BadRequest('Invalid activation link');
     user.isActivated = true;
     await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({email})
+    if (!user) {
+        throw ApiError.BadRequest('Not found with this email')
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+        throw ApiError.BadRequest('Invalid password');
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({...userDto});
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return {...tokens, user: userDto}
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
   }
 }
 
